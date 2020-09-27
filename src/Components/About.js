f7e86836e7992516e68ef11ece57a13d105f0e30
hardiksingh98 <hardik.singh@turtlemint.com>
2020-09-27 12:55:55 +0530

    resume download fix

--- src/Components/About.js
+++ src/Components/About.js
@@ -44,7 +44,7 @@ class About extends Component {
                </div>
                <div className="columns download">
                   <p>
-                     <a href={pdf} className="button" target="_blank"><i className="fa fa-download"></i>Download Resume</a>
+                     <a href={pdf} className="button" download={pdf}><i className="fa fa-download"></i>Download Resume</a>
                   </p>
                </div>
             </div>
