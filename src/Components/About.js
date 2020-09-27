5d026c1e5bc635c93099d69d1c1e005e6694ea36
hardiksingh98 <hardik.singh@turtlemint.com>
2020-09-27 10:11:17 +0530

    first commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import pdf from './Hardik_Singh.pdf';
+
 
 class About extends Component {
   render() {
 
     if(this.props.data){
       var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
+      var profilepic= require("../"+this.props.data.image);
       var bio = this.props.data.bio;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
@@ -13,19 +15,21 @@ class About extends Component {
       var zip = this.props.data.address.zip;
       var phone= this.props.data.phone;
       var email = this.props.data.email;
-      var resumeDownload = this.props.data.resumedownload;
+
+
     }
 
     return (
       <section id="about">
       <div className="row">
-         <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
-         </div>
+         <div className="nine columns main-cols">
+            <img className="profile-pic"  src={profilepic} alt="Hardik Singh" />
          <div className="nine columns main-col">
             <h2>About Me</h2>
 
             <p>{bio}</p>
+
+         </div>
             <div className="row">
                <div className="columns contact-details">
                   <h2>Contact Details</h2>
@@ -40,7 +44,7 @@ class About extends Component {
                </div>
                <div className="columns download">
                   <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
+                     <a href={pdf} className="button" target="_blank"><i className="fa fa-download"></i>Download Resume</a>
                   </p>
                </div>
             </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -35,7 +35,7 @@ class Header extends Component {
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
+            <h3>Developer with strong passion for self-improvement. Like to solve problems & bring energy, patience, positivity and cultural values to the team.</h3>
             <hr />
             <ul className="social">
                {networks}
